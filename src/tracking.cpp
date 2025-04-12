4f8d4027ec5526269279431f783b00d5da030634
alexcubor <sdiz@ya.ru>
2025-04-12 10:15:13 +0300

    Remover lower name

--- src/tracking.cpp
+++
@@ -1,3 +0,0 @@
-// Performs keypoint detection using ORB.
-// Tracks point motion between frames (optical flow, for example, Lucas-Kanade).
-// Estimates camera position (Camera Pose Estimation) using PnP (Perspective-n-Point).
