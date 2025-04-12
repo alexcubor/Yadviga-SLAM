779eaf0323db36e42d5da402b1aa96da135ae315
alexcubor <sdiz@ya.ru>
2025-04-12 10:15:57 +0300

    Add Tracking.cpp file

---
+++ src/Tracking.cpp
@@ -0,0 +1,3 @@
+// Performs keypoint detection using ORB.
+// Tracks point motion between frames (optical flow, for example, Lucas-Kanade).
+// Estimates camera position (Camera Pose Estimation) using PnP (Perspective-n-Point).
